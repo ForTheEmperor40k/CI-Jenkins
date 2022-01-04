597669540b2c68d23b78c83d734420804bae866f
Makoveev Dima <hakerprovider@gmail.com>
2022-01-04 15:15:41 +0300

    New init

---
+++ MsTestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Aquality.Selenium.Browsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace MsTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var url = Environment.GetEnvironmentVariable("url");
+            AqualityServices.Browser.GoTo(url);
+            AqualityServices.Browser.Maximize();
+
+            byte[] image = AqualityServices.Browser.GetScreenshot();
+            using (var memory = new MemoryStream(image))
+            {
+                Image imageIn = Image.FromStream(memory);
+                Bitmap bmp = new Bitmap(imageIn);
+                bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+            }
+
+            AqualityServices.Browser.Quit();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var currentUrl = Environment.GetEnvironmentVariable("url");
+            string expectedUrl = "https://www.a1qa.by/";
+            Assert.AreEqual(expectedUrl, currentUrl, "Links don't match");
+        }
+    }
+}
