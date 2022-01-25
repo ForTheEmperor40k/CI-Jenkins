44bba49cb739f9718daaae796318e19ff99874db
Makoveev Dima <hakerprovider@gmail.com>
2022-01-25 12:55:07 +0300

    Test param

--- MsTestProject/UnitTest1.cs
+++ MsTestProject/UnitTest1.cs
@@ -13,49 +13,49 @@ namespace MsTestProject
     {
         private string url = Environment.GetEnvironmentVariable("testUrl");
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            AqualityServices.Browser.GoTo(url);
-            AqualityServices.Browser.Maximize();
-            string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\") + 
-                @"Screenshots\");
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    AqualityServices.Browser.GoTo(url);
+        //    AqualityServices.Browser.Maximize();
+        //    string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\") + 
+        //        @"Screenshots\");
 
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
+        //    if (!Directory.Exists(path))
+        //    {
+        //        Directory.CreateDirectory(path);
+        //    }
 
-            byte[] image = AqualityServices.Browser.GetScreenshot();
-            using (var memory = new MemoryStream(image))
-            {
-                Image imageIn = Image.FromStream(memory);
-                Bitmap bmp = new Bitmap(imageIn);
-                bmp.Save(path + "1.png", System.Drawing.Imaging.ImageFormat.Png);
-            }
+        //    byte[] image = AqualityServices.Browser.GetScreenshot();
+        //    using (var memory = new MemoryStream(image))
+        //    {
+        //        Image imageIn = Image.FromStream(memory);
+        //        Bitmap bmp = new Bitmap(imageIn);
+        //        bmp.Save(path + "1.png", System.Drawing.Imaging.ImageFormat.Png);
+        //    }
 
-            AqualityServices.Browser.Quit();
-        }
+        //    AqualityServices.Browser.Quit();
+        //}
 
-        [TestMethod]
-        public void TestMethod2()
-        {
-            string expectedUrl = "https://www.a1qa.by/";
-            Assert.AreEqual(expectedUrl, url, "Links don't match");
-        }
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //    string expectedUrl = "https://www.a1qa.by/";
+        //    Assert.AreEqual(expectedUrl, url, "Links don't match");
+        //}
 
-        [TestMethod]
-        [Ignore("Ignore this test")]
-        public void IgnoreTest()
-        {
-            Assert.IsTrue(true, "Not true");
-        }
+        //[TestMethod]
+        //[Ignore("Ignore this test")]
+        //public void IgnoreTest()
+        //{
+        //    Assert.IsTrue(true, "Not true");
+        //}
 
-        [TestMethod]
-        public void FailTest()
-        {
-            Assert.Fail("Test failed");
-        }
+        //[TestMethod]
+        //public void FailTest()
+        //{
+        //    Assert.Fail("Test failed");
+        //}
 
         [TestMethod]
         public void GetDataTest()
@@ -63,8 +63,8 @@ namespace MsTestProject
             const string login = "mail@mail.com";
             const string password = "qwerty123";
 
-            Assert.IsTrue(login.Contains(TestDataModel.Login), $"Logins do not match. But was -{TestDataModel.Login}");
-            Assert.IsTrue(password.Contains(TestDataModel.Password), $"Passwords do not match. But was -{TestDataModel.Password}");
+            Assert.AreEqual(login, TestDataModel.Login, $"Logins do not match. But was -{TestDataModel.Login}");
+            Assert.AreEqual(password, TestDataModel.Password, $"Logins do not match. But was -{TestDataModel.Password}");
         }
     }
 }
